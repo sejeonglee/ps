04549cab3ae68cbbf9141d36be2f05f649670bcb
sejeonglee <sejeonglee2@gmail.com>
2022-09-23 00:33:16 +0900

    Fix submit template codes

---
+++ templates/codeforces.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void process() {
+  int answer;
+
+  cout << answer << "\n";
+}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+
+  int T;
+  cin >> T;
+
+  for (int i = 1; i <= T; i++) {
+    process();
+  }
+
+  return 0;
+}
